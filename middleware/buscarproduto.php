dd44f2bffb8833a29d37462a7a83741114d03af2
Koda <andeyaelydio@gmail.com>
2024-11-22 11:57:06 -0300

    1.0.3

--- middleware/buscarproduto.php
+++ middleware/buscarproduto.php
@@ -1,5 +1,8 @@
 <?php
     $produto = [];
     if(isset($_GET['edit'])){    
+        // busca o produto
     $produto = $database->buscar($_GET['edit']);
+        // transforma de objeto mysql para array
+    $produto = $produto->fetch_assoc();
 }
